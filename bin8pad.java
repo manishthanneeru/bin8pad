e442ad91c1216907f23b209607de99487486e93e
manishthanneeru <manish.thanneeru@gmail.com>
2017-03-26 11:00:45 +0530

    bin8pad.java
    This function asks user for a binary number and pads 0's according to his need.

---
+++ bin8pad.java
@@ -0,0 +1,46 @@
+import java.lang.*;
+import java.io.*;
+public class bin8pad
+{
+public static void main(String [] args)
+{
+BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));   //reads input from user
+try {
+    String x= sc.readLine();
+    String bytecodes=bin(x);                                            //calls encode function try block
+    System.out.println("The resultant String is:" +bytecodes);
+} 
+catch (IOException e) 
+{
+   e.printStackTrace(); //handle exception
+}
+}
+public static String bin(String ip)
+{
+    String sel=ip;
+    int l=0;
+    BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter the length of binary variable you want");
+    try{
+    l=Integer.parseInt(sc.readLine());
+} 
+catch(IOException e){
+    e.printStackTrace();
+}
+        if(sel.length()<l){
+            int tmp=sel.length();
+            while(l-tmp>0)
+            {
+                sel="0"+sel;
+                tmp++;
+            }   
+        }
+        else if(sel.length()>l)
+        {
+          System.err.println("Given Binary out of Bounds");
+        }
+       return sel;
+    }
+    
+}
+
